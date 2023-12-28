ebc7cb00d9c175a0528fc6b0460840210964117f
Karagulamos <karagulamos@gmail.com>
2023-12-28 07:13:21 +0100

    feat: execution tracking middlware

---
+++ Taxer.Web.API/Middleware/ExecutionTrackerMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace Taxer.Web.API.Middleware;
+
+public class ExecutionTrackerMiddleware(ILogger<ExecutionTrackerMiddleware> logger) : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        // Start timing of the request
+        var watch = Stopwatch.StartNew();
+
+        // Continue processing the request using the next middleware in the pipeline
+        await next(context);
+
+        // Stop timing of the request
+        watch.Stop();
+
+        // Calculate the elapsed time
+        var elapsedTime = watch.Elapsed;
+
+        // Log the execution time of the request
+        logger.LogInformation(
+            "Request: {method} {path} completed in {elapsedTime:hh\\:mm\\:ss\\.fff}",
+            context.Request.Method,
+            context.Request.Path,
+            elapsedTime
+        );
+    }
+}
+
